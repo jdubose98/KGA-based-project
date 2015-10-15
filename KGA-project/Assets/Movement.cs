226c0b4f698ebfd086fbb36a3849e5e0e493022d
Jacob Dubose <jdubose@IT302.mr.preble.com>
2015-10-15 10:30:55 -0400

    yet another change

---
+++ KGA-project/Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var tgt = GameObject.Find("Player")
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Vertical") != 0){
+		if (Input.GetAxis ("Vertical" > 0))
+			;
+		}
+	}
+}
