b44cca8a2e126d75226364215d34138d60aae2af
Jacob Dubose <jdubose@utc4me.org>
2015-10-26 10:35:24 -0400

    lotsa changes
    maybe one day i'll make this silly laser work

--- KGA-project/Assets/Scripts/PlayerController_Spaceship.cs
+++ KGA-project/Assets/Scripts/PlayerController_Spaceship.cs
@@ -11,6 +11,10 @@ public class PlayerController_Spaceship : MonoBehaviour {
 	public float TimeLeftOnShield = 10f; // A hard limit
 	public float ShieldDecayRate = .1f; // How much shield we steal every "tick" defined in the tickrate
 	public float ShieldDecayTickrate = 1f; // Per second decay rate, for finer tuning
+	public float FireSpeed = 0.5f; // Player's fire speed per second
+	public Rigidbody2D ProjectilePrefab;
+	public Transform ProjectileSourceZone;
+	private float lastFiredTime = 0; // Sets this variable up early on
 
 	// Define sounds
 	public AudioSource FireSound;
@@ -24,8 +28,14 @@ public class PlayerController_Spaceship : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
-			Debug.Log ("Fire button is down");
+		if (Input.GetButtonDown ("Fire1")) { // checks to see if the player is firing
+			Debug.Log ("Fire button is down"); // just debugging
+			float curTime = Time.time; // captures the current time
+			if ((curTime - lastFiredTime) > FireSpeed){ // determines if it's been long enough so we can fire again
+				Rigidbody2D projectile;
+				projectile = Instantiate(ProjectilePrefab, ProjectileSourceZone.position, ProjectileSourceZone.rotation) as Rigidbody2D;
+				projectile.AddForce(ProjectileSourceZone.forward*100,ForceMode2D.Impulse);
+			}
 		}
 		else if (Input.GetAxis ("Vertical") !=0 || Input.GetAxis ("Horizontal") !=0 )
 		{
