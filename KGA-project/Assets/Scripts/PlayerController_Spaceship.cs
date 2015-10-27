a2008a69e40cab448911f936bfc25d2931a99bfa
Jacob Dubose <jdubose@utc4me.org>
2015-10-27 10:11:03 -0400

    holy fixes batman
    just need to fix cursor tracking

--- KGA-project/Assets/Scripts/PlayerController_Spaceship.cs
+++ KGA-project/Assets/Scripts/PlayerController_Spaceship.cs
@@ -3,14 +3,21 @@ using System.Collections;
 
 public class PlayerController_Spaceship : MonoBehaviour {
 
-	//Define player variables
-	public float PlayerSpeed = 1f; // The speed of the player
+    //Define player variables
+    public bool Shielded = false; // Duh
+    public int ShieldStrength = 3; // How much "power" the shield has
+    public float TimeLeftOnShield = 10f; // A hard limit
+    public float ShieldDecayRate = .1f; // How much shield we steal every "tick" defined in the tickrate
+    public float ShieldDecayTickrate = 1f; // Per second decay rate, for finer tuning
+
+    // Uncategorized
+    public float PlayerSpeed = 1f; // The speed of the player
 	public int DamageLevel = 0; // The damage state of the player's craft (up to 3)
-	public bool Shielded = false; // Duh
-	public int ShieldStrength = 3; // How much "power" the shield has
-	public float TimeLeftOnShield = 10f; // A hard limit
-	public float ShieldDecayRate = .1f; // How much shield we steal every "tick" defined in the tickrate
-	public float ShieldDecayTickrate = 1f; // Per second decay rate, for finer tuning
+    public SpriteRenderer ThrusterRenderer;
+    public SpriteRenderer ShieldRenderer;
+    public SpriteRenderer DamageOverlay;
+
+	// Projectile stuff ok
 	public float FireSpeed = 0.5f; // Player's fire speed per second
 	public Rigidbody2D ProjectilePrefab;
 	public Transform ProjectileSourceZone;
@@ -20,6 +27,7 @@ public class PlayerController_Spaceship : MonoBehaviour {
 	public AudioSource FireSound;
 	public AudioSource ShieldHitSound;
 	public AudioSource PlayerDeathSound;
+    public AudioSource ThrusterSound;
 
 	// Use this for initialization
 	void Start () {
@@ -32,16 +40,38 @@ public class PlayerController_Spaceship : MonoBehaviour {
 			Debug.Log ("Fire button is down"); // just debugging
 			float curTime = Time.time; // captures the current time
 			if ((curTime - lastFiredTime) > FireSpeed){ // determines if it's been long enough so we can fire again
-				Rigidbody2D projectile;
-				projectile = Instantiate(ProjectilePrefab, ProjectileSourceZone.position, ProjectileSourceZone.rotation) as Rigidbody2D;
-				projectile.AddForce(ProjectileSourceZone.forward*100,ForceMode2D.Impulse);
+				//Rigidbody2D projectile;
+				//projectile = Instantiate(ProjectilePrefab, ProjectileSourceZone.position, ProjectileSourceZone.rotation) as Rigidbody2D;
+				//projectile.AddForce(ProjectileSourceZone.forward*100,ForceMode2D.Impulse);
 			}
 		}
-		else if (Input.GetAxis ("Vertical") !=0 || Input.GetAxis ("Horizontal") !=0 )
+
+        // Control player movement
+        var vect = new Vector3(Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"), 0); // Gets a new vector using the axes we're moving on.
 		{
-			transform.Translate(0,Input.GetAxis("Vertical")*PlayerSpeed,0);
-			transform.Translate(Input.GetAxis("Horizontal")*PlayerSpeed,0,0);
+			transform.position = transform.position + (PlayerSpeed * vect.normalized * Time.deltaTime); // normalized vector (no cheating!) is mult. by the speed variable, then mult. by deltaTime to keep the speed consistent even under lag.
 		}
+
+        // Handle displaying of thruster tail
+        if (Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0) // Choosing raw because it cuts the thruster out and in instantly
+        {
+            ThrusterRenderer.enabled = true;
+            if (ThrusterSound.isPlaying)
+            ThrusterSound.Play();
+        }
+        else
+        {
+            ThrusterRenderer.enabled = false;
+            if (ThrusterSound.isPlaying)
+                ThrusterSound.Stop();
+        }
+
+        // Rotate player towards cursor
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition - pos;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg; // i admit to copypasta on this line.
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
 	}
 
 }
