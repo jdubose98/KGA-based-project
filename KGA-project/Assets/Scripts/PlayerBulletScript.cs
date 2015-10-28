10e2809a4f714bbd146d28f6128835533784caff
Jacob Dubose <jdubose@utc4me.org>
2015-10-28 10:32:13 -0400

    The player is functional now
    It shoots lasers! And flies!

--- KGA-project/Assets/Scripts/PlayerBulletScript.cs
+++ KGA-project/Assets/Scripts/PlayerBulletScript.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class PlayerBulletScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    static float ProjectileLife = 2; // Projectile life in seconds
+    [SerializeField] float ProjectileSpeed = 6; // Bullet speed
+    float Timer = Time.time + ProjectileLife; // Kinda cheaty
+
+    // Use this for initialization
+    void Start () {
+        Destroy(gameObject, 2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        var projectile = gameObject.GetComponent<Rigidbody2D>();
+            projectile.transform.Translate(0, ProjectileSpeed * Time.deltaTime,0);
+
+    }
 }
--- KGA-project/Assets/Scripts/PlayerController_Spaceship.cs
+++ KGA-project/Assets/Scripts/PlayerController_Spaceship.cs
@@ -4,30 +4,30 @@ using System.Collections;
 public class PlayerController_Spaceship : MonoBehaviour {
 
     //Define player variables
-    public bool Shielded = false; // Duh
-    public int ShieldStrength = 3; // How much "power" the shield has
-    public float TimeLeftOnShield = 10f; // A hard limit
-    public float ShieldDecayRate = .1f; // How much shield we steal every "tick" defined in the tickrate
-    public float ShieldDecayTickrate = 1f; // Per second decay rate, for finer tuning
+    [SerializeField] bool Shielded = false; // Duh
+    [SerializeField] int ShieldStrength = 3; // How much "power" the shield has
+    [SerializeField] float TimeLeftOnShield = 10f; // A hard limit
+    [SerializeField] float ShieldDecayRate = .1f; // How much shield we steal every "tick" defined in the tickrate
+    [SerializeField] float ShieldDecayTickrate = 1f; // Per second decay rate, for finer tuning
 
     // Uncategorized
-    public float PlayerSpeed = 1f; // The speed of the player
-	public int DamageLevel = 0; // The damage state of the player's craft (up to 3)
-    public SpriteRenderer ThrusterRenderer;
-    public SpriteRenderer ShieldRenderer;
-    public SpriteRenderer DamageOverlay;
+    [SerializeField] float PlayerSpeed = 1f; // The speed of the player
+	[SerializeField] int DamageLevel = 0; // The damage state of the player's craft (up to 3)
+    [SerializeField] SpriteRenderer ThrusterRenderer;
+    [SerializeField] SpriteRenderer ShieldRenderer;
+    [SerializeField] SpriteRenderer DamageOverlay;
 
 	// Projectile stuff ok
-	public float FireSpeed = 0.5f; // Player's fire speed per second
-	public Rigidbody2D ProjectilePrefab;
-	public Transform ProjectileSourceZone;
+	[SerializeField] float FireSpeed = 0.5f; // Player's fire speed per second
+	[SerializeField] Rigidbody2D ProjectilePrefab;
+	[SerializeField] Transform ProjectileSourceZone;
 	private float lastFiredTime = 0; // Sets this variable up early on
 
 	// Define sounds
-	public AudioSource FireSound;
-	public AudioSource ShieldHitSound;
-	public AudioSource PlayerDeathSound;
-    public AudioSource ThrusterSound;
+	[SerializeField] AudioSource FireSound;
+	[SerializeField] AudioSource ShieldHitSound;
+	[SerializeField] AudioSource PlayerDeathSound;
+    [SerializeField] AudioSource ThrusterSound;
 
 	// Use this for initialization
 	void Start () {
@@ -40,9 +40,10 @@ public class PlayerController_Spaceship : MonoBehaviour {
 			Debug.Log ("Fire button is down"); // just debugging
 			float curTime = Time.time; // captures the current time
 			if ((curTime - lastFiredTime) > FireSpeed){ // determines if it's been long enough so we can fire again
-				//Rigidbody2D projectile;
-				//projectile = Instantiate(ProjectilePrefab, ProjectileSourceZone.position, ProjectileSourceZone.rotation) as Rigidbody2D;
-				//projectile.AddForce(ProjectileSourceZone.forward*100,ForceMode2D.Impulse);
+                FireSound.Play();
+				Rigidbody2D projectile;
+				projectile = Instantiate(ProjectilePrefab, ProjectileSourceZone.position, ProjectileSourceZone.rotation) as Rigidbody2D;
+				projectile.AddForce(ProjectileSourceZone.forward*10000,ForceMode2D.Impulse);
 			}
 		}
 
@@ -57,7 +58,7 @@ public class PlayerController_Spaceship : MonoBehaviour {
         if (Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0) // Choosing raw because it cuts the thruster out and in instantly
         {
             ThrusterRenderer.enabled = true;
-            if (ThrusterSound.isPlaying)
+            if (!ThrusterSound.isPlaying)
             ThrusterSound.Play();
         }
         else
@@ -70,8 +71,9 @@ public class PlayerController_Spaceship : MonoBehaviour {
         // Rotate player towards cursor
         Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
         Vector3 dir = Input.mousePosition - pos;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg; // i admit to copypasta on this line.
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        float angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) -90; // i admit to copypasta on this line. the -90 corrects the rotation
+                                                                       // programmers who actually took math classes made math libraries for us people...
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); // grabs our quaternion so we can rotate the character in world space.
 
 	}
 
