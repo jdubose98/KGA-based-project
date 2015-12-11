2b6be45c2daa1850cd0a0f87eac71e2a0af18b46
Jacob Dubose <jdubose@IT201.mr.preble.com>
2015-12-11 09:25:16 -0500

    Minimap made
    But can one make it circular?

---
+++ CIPs/Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollower : MonoBehaviour {
+
+    [SerializeField]
+    Transform Target;
+
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = new Vector3(Target.transform.position.x, Target.transform.position.y, 0);
+	}
+}
