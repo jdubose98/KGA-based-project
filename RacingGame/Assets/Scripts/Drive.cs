64ed591dfe32bc9578e7362cdd65a106c0d14c03
Jacob Dubose <jdubose@IT201.mr.preble.com>
2016-01-08 10:18:46 -0500

    Added sounds
    BRAAAAAAAAM

---
+++ RacingGame/Assets/Scripts/Drive.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drive : MonoBehaviour {
+
+    // **** Semi-private variables **** //
+
+    // Properties for driving
+    [SerializeField] float DriveSpeed;
+    [SerializeField] float TurnSpeed;
+    // Audio
+    [SerializeField] AudioSource ThrottleInSource;
+    [SerializeField] AudioSource ThrottleOutSource;
+    [SerializeField] AudioSource SkidSource;
+
+    // ****** Private variables ****** //
+    float forceMultiplier;
+    public float speed;
+    Vector3 lastPosition;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+    void FixedUpdate()
+    {
+        speed = (transform.position - lastPosition).magnitude;
+        lastPosition = transform.position;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2 (0, DriveSpeed * Input.GetAxis("Vertical")*forceMultiplier));
+        gameObject.transform.Rotate(new Vector3(0, 0, TurnSpeed * -Input.GetAxis("Horizontal") * Time.deltaTime));
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            if (forceMultiplier <= 1) {
+                forceMultiplier = forceMultiplier + 0.01f;
+                ThrottleInSource.volume = ThrottleInSource.volume + 0.05f;
+                ThrottleOutSource.volume = ThrottleOutSource.volume - 0.025f;
+            }
+            else forceMultiplier = 1;
+        }
+        else
+        {
+            if (forceMultiplier > 0) {
+                forceMultiplier = forceMultiplier / 3;
+                ThrottleOutSource.volume = ThrottleOutSource.volume + 0.05f;
+                ThrottleInSource.volume = ThrottleInSource.volume - 0.025f;
+            }
+            else if (forceMultiplier <= 0)
+                forceMultiplier = 0; // safety net
+        }
+
+        ThrottleInSource.pitch = .8f + (speed * 4f); // Set pitch
+        ThrottleOutSource.pitch = .8f + (speed * 4f); // Set pitch
+
+        if (speed*5 > .8)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                SkidSource.volume = SkidSource.volume + 0.075f;
+            }
+            else
+                SkidSource.volume = SkidSource.volume - 0.075f;
+        }
+        else
+            SkidSource.volume = SkidSource.volume - 0.075f;
+    }
+}
---
+++ RacingGame/Assets/Scripts/Follower.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follower : MonoBehaviour {
+
+    [SerializeField] Transform Target; // Object we're following (in this case the player)
+    float smoothingFactor; // Multiplier for zoom rate
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(Target.position.x, Target.position.y, -10), 1f); // Tween camera position
+
+        if (5.25f + (2.25f * smoothingFactor) > 7.25) // Is our zoom size greater than 7.25?
+            gameObject.GetComponent<Camera>().orthographicSize = 7.25f; // Hard cap on camera zoom to prevent jittering
+        else
+            gameObject.GetComponent<Camera>().orthographicSize = 5.25f + (2.25f * smoothingFactor); // Otherwise zoom as normal
+
+        smoothingFactor = Target.GetComponent<Drive>().speed*5; // Finds out our smoothing factor
+	}
+}
